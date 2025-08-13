19e25f93f9cb3ca00e90a2d4d26c556545bfebb2
Caleb Uhouse <calebmuhouse@gmail.com>
2025-08-13 09:26:08 -0400

    Updated formatting and fixed card block issue where image was too big

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,56 +4,58 @@ import cardApi from '../services/Api'
 import CardBlock from '../common/components/CardBlock'
 
 const App = () => {
-  const [searchValue, setSearchValue] = useState('')
-  const [cardResults, setCardResults] = useState({ data: { cards: [] } })
-  const [numberDisplayed, setNumberDisplayed] = useState(5)
+    const [searchValue, setSearchValue] = useState('')
+    const [cardResults, setCardResults] = useState({ data: { cards: [] } })
+    const [numberDisplayed, setNumberDisplayed] = useState(5)
 
-  const getCardByName = (cardName: string) => {
-    cardApi.get(`/cards?name=${cardName}`).then((response) => {
-      setCardResults(response)
-    })
-  }
+    const getCardByName = (cardName: string) => {
+        cardApi.get(`/cards?name=${cardName}`).then((response) => {
+            setCardResults(response)
+        })
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="card-finder-container">
-          <form onSubmit={(e) => e.preventDefault()}>
-            <input
-              className="search-value"
-              type="text"
-              placeholder="Enter Name of Card"
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-            />
-            <button
-              className="search-value"
-              onClick={() => getCardByName(searchValue)}
-            >
-              Search
-            </button>
-          </form>
-          <div className="results-container">
-            <h1>Cards:</h1>
-            <p>Number of Cards Shown:</p>
-            <input
-              className="number-displayed"
-              type="number"
-              value={numberDisplayed}
-              onChange={(e) => setNumberDisplayed(parseInt(e.target.value))}
-            />
-            <div className="card-block-container">
-              {cardResults.data?.cards
-                .slice(0, numberDisplayed)
-                .map((card, index) => (
-                  <CardBlock key={index} cardData={card} />
-                ))}
-            </div>
-          </div>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="card-finder-container">
+                    <form onSubmit={(e) => e.preventDefault()}>
+                        <input
+                            className="search-value"
+                            type="text"
+                            placeholder="Enter Name of Card"
+                            value={searchValue}
+                            onChange={(e) => setSearchValue(e.target.value)}
+                        />
+                        <button
+                            className="search-value"
+                            onClick={() => getCardByName(searchValue)}
+                        >
+                            Search
+                        </button>
+                    </form>
+                    <div className="results-container">
+                        <h1>Cards:</h1>
+                        <p>Number of Cards Shown:</p>
+                        <input
+                            className="number-displayed"
+                            type="number"
+                            value={numberDisplayed}
+                            onChange={(e) =>
+                                setNumberDisplayed(parseInt(e.target.value))
+                            }
+                        />
+                        <div className="card-block-container">
+                            {cardResults.data?.cards
+                                .slice(0, numberDisplayed)
+                                .map((card, index) => (
+                                    <CardBlock key={index} cardData={card} />
+                                ))}
+                        </div>
+                    </div>
+                </div>
+            </header>
         </div>
-      </header>
-    </div>
-  )
+    )
 }
 
 export default App
--- src/common/components/CardBlock/index.tsx
+++ src/common/components/CardBlock/index.tsx
@@ -1,13 +1,13 @@
-import './index.css';
+import './index.css'
 
 const CardBlock = (props: any) => {
-  return (
-    <div className="card-block">
-      <img src={props.cardData.imageUrl} alt="Card Art Not Available" />
-      <h5>{props.cardData.name}</h5>
-      <p>Set: {props.cardData.setName}</p>
-    </div>
-  );
-};
+    return (
+        <div className="card-block">
+            <img src={props.cardData.imageUrl} alt="Card Art Not Available" />
+            <h5>{props.cardData.name}</h5>
+            <p>Set: {props.cardData.setName}</p>
+        </div>
+    )
+}
 
-export default CardBlock;
+export default CardBlock
